64e98958bb8cfffe7705c530540b7fb075fb2353
eric373 <25942990+eric373@users.noreply.github.com>
2022-05-26 15:13:11 -0400

    Add configuration for delta lake

--- scripts/ss.ps1
+++ scripts/ss.ps1
@@ -4,6 +4,8 @@ if($args.Length -gt 0)
     spark-submit --master local `
         --packages io.delta:delta-core_2.12:0.7.0 `
         --jars "D:\Spark\sqljdbc_10.2\enu\mssql-jdbc-10.2.0.jre8.jar" `
+        --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" `
+        --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" `
         $args[0]
         # $args[0] `
         # $($Env:SPARK_HOME + "\README.md")
