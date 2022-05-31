17b9e7187ba7029eb95991f3f10c5a0a65a6228d
eric373 <25942990+eric373@users.noreply.github.com>
2022-05-31 11:53:09 -0400

    Incorporate Spark Structured Streaming to use Kafka as source

--- scraping/indeed/consumer.py
+++ scraping/indeed/consumer.py
@@ -14,12 +14,12 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import json
 
 from scrape import getSoup
 
-# TODO: ERROR: KafkaError{code=_MAX_POLL_EXCEEDED,val=-147,str="Application maximum poll interval (300000ms) exceeded by 406ms"}
 # TODO: Instead of retrieving the HTML of a posting, read this from a Delta Lake to simulate accessing the HTML page.
-# TODO: Use Spark Structured Streaming to read from a kafka topic. Use a UDF (getSavePosting) to retrieve the HTML of the job posting and save this to a Delta Lake.
+# TODO: Use a UDF (getSavePosting) to retrieve the HTML of the job posting and save this to a Delta Lake.
 
 if __name__ == '__main__':
     # Parse the command line.
@@ -68,42 +68,26 @@ if __name__ == '__main__':
         df = spark.sparkContext.parallelize(ls).toDF(colNames)
         df.write.format("delta").mode("append").save(deltaPath)
 
-    # Poll for new messages from Kafka and print them.
-    ls = []
-    colNames = ["JobTitle", "CompanyName", "Location", "Url", "Hash"]
-    try:
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is None:
-                # Initial message consumption may take up to
-                # `session.timeout.ms` for the consumer group to
-                # rebalance and start consuming
-                if len(ls) > 0:
-                    with webdriver.Chrome() as driver:
-                        start = 0
-                        N = len(ls)
-                        while start + 5 < N:
-                            getSavePosting(ls[start : start + 5])
-                            start = start + 5
-                        getSavePosting(ls[start:])
-                    ls.clear()
-                print("Waiting...")
-            elif msg.error():
-                print("ERROR: {0}".format(msg.error()))
-            else:
-                # Extract the (optional) key and value, and print.
-                key = msg.key()
-                if key is not None:
-                    key = key.decode('utf-8')
-                else:
-                    key = ''
-                text = msg.value().decode("utf-8")
-                ls.append(re.findall(r'"(.*?)"', text))
-                # print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
-                #     topic=msg.topic(), key = key, value = text))
-    except KeyboardInterrupt:
-        pass
-    finally:
-        # Leave group and commit final offsets
-        consumer.close()
-        spark.stop()
+    # Use kafka as source for Spark Structured Streaming
+    offsets = {"jobs": {"0": 1335}}
+    streaming = spark.readStream.format("kafka")\
+        .option("kafka.bootstrap.servers", config["bootstrap.servers"])\
+        .option("subscribe", "jobs")\
+        .option("maxOffsetsPerTrigger", 1)\
+        .option("startingOffsets", json.dumps(offsets))\
+        .load()
+
+    # Apply some transformations on the stream
+    # nothing
+
+    # Specify in-memory table as sink and start the stream in the background
+    query = streaming.writeStream.queryName("records")\
+        .format("memory").outputMode("append")\
+        .start()
+
+    while query.status["isTriggerActive"]:
+        print("{0} - {1}".format(query.status, spark.sql("SELECT * FROM records").count()))
+        time.sleep(1)
+    
+    # Prevent driver process from exiting while query is still running
+    query.awaitTermination()
--- scripts/ss.ps1
+++ scripts/ss.ps1
@@ -1,11 +1,17 @@
 Clear-Host
+$packages = @(
+    "io.delta:delta-core_2.12:0.7.0",
+    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3"
+) -join ","
+
 if($args.Length -gt 0)
 {
     spark-submit --master local `
-        --packages io.delta:delta-core_2.12:0.7.0 `
+        --packages $packages `
         --jars "D:\Spark\sqljdbc_10.2\enu\mssql-jdbc-10.2.0.jre8.jar" `
         --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" `
         --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" `
+        --conf "spark.sql.shuffle.partitions=5" `
         @args # splat the $args array
         # $args[0] `
         # $($Env:SPARK_HOME + "\README.md")
