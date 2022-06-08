4e3d0c6b389c9f650c56fd5ff967bf8d2d3ccd3a
eric373 <25942990+eric373@users.noreply.github.com>
2022-06-08 12:06:08 -0400

    Use pyspark UDF to retrieve HTML of job posting

--- scraping/indeed/consumer.py
+++ scraping/indeed/consumer.py
@@ -1,8 +1,8 @@
 #python consumer.py --config_file config.ini --reset
 #ss consumer.py --config_file config.ini --reset
+#watchFiles -Command ss -FileFiter
 # A script that subscribes to a kafka topic and retrieves the HTML list for a
 # job listing.
-import sys
 from argparse import ArgumentParser, FileType
 from configparser import ConfigParser
 from confluent_kafka import Consumer, OFFSET_BEGINNING
@@ -10,84 +10,93 @@ import re
 from pyspark.sql import SparkSession
 from delta import *
 import random
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from pyspark.sql.utils import AnalysisException
 import time
-import json
+from selenium import webdriver
+from pyspark.sql.streaming import StreamingQuery
+import datetime
+from time import sleep
+from pyspark.sql.functions import regexp_extract, udf
+from pyspark.sql.types import StringType
 
 from scrape import getSoup
 
-# TODO: Instead of retrieving the HTML of a posting, read this from a Delta Lake to simulate accessing the HTML page.
-# TODO: Use a UDF (getSavePosting) to retrieve the HTML of the job posting and save this to a Delta Lake.
-
 if __name__ == '__main__':
-    # Parse the command line.
+    # Parse Kafka configuration file to get bootstrap server
     parser = ArgumentParser()
     parser.add_argument('--config_file', type = FileType('r'))
     parser.add_argument('--reset', action = 'store_true')
     args = parser.parse_args()
-
-    # Parse the configuration.
-    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
     config_parser = ConfigParser()
     config_parser.read_file(args.config_file)
     config = dict(config_parser['default'])
     config.update(config_parser['consumer'])
 
-    # Create Consumer instance
-    consumer = Consumer(config)
-
-    # Set up a callback to handle the '--reset' flag.
-    def reset_offset(consumer, partitions):
-        if args.reset:
-            for p in partitions:
-                p.offset = OFFSET_BEGINNING
-            consumer.assign(partitions)
-
-    # Subscribe to topic
-    topics = ["jobs"]
-    consumer.subscribe(topics, on_assign = reset_offset)
-
     # Create an entry point for spark
-    spark = SparkSession.builder.appName("Test") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .getOrCreate()
-    deltaPath = "D:\\deltalakes\\jobs\\"
+    spark = SparkSession.builder.appName("Test").getOrCreate()
 
-    # Given a list of url's 
-    def getSavePosting(ls: list):
-        for entry in ls:
-            url = entry[-2]
-            soup = getSoup(url, driver)
-            entry.append(soup.prettify())
-            ts = random.randint(1,10) + random.random()
-            print(f"Retrieved job posting for {entry[0]} --- {entry[1]} --- {entry[2]}. Sleeping for {ts:.2f}s")
-            time.sleep(ts)
-        df = spark.sparkContext.parallelize(ls).toDF(colNames)
-        df.write.format("delta").mode("append").save(deltaPath)
+    # Function to terminate query when no new data has arrived in timeout seconds
+    def timeoutNewData(query: StreamingQuery, timeout: int):
+        newData = datetime.datetime.now()
+        while True:
+            sleep(1)
+            dct = query.lastProgress
+            if dct is not None:
+                if dct["numInputRows"] != 0:
+                    newData = datetime.datetime.now()
+                if (datetime.datetime.now() - newData).total_seconds() >= timeout:
+                    break
+
+    # User-defined function to get job posting HTML
+    def getSoupDriver(url: str):
+        with webdriver.Chrome() as driver:
+            return getSoup(url, driver).prettify()
 
     # Use kafka as source for Spark Structured Streaming
-    offsets = {"jobs": {"0": 1335}}
     streaming = spark.readStream.format("kafka")\
         .option("kafka.bootstrap.servers", config["bootstrap.servers"])\
         .option("subscribe", "jobs")\
         .option("maxOffsetsPerTrigger", 1)\
-        .option("startingOffsets", json.dumps(offsets))\
+        .option("startingOffsets","earliest")\
         .load()
 
-    # Apply some transformations on the stream
-    # nothing
+    # Check if Delta Lake already exists to prevent overwrite
+    deltaPath = "D:\\deltalakes\\jobs\\"
+    checkPointLocation = "D:\\deltalakes\\jobsCheckpoint\\"
+    write = False
+    try:
+        if spark.read.format("delta")\
+            .option("path", deltaPath).load().count() != 0:
+            write = True
+    except AnalysisException:
+        write = True
 
-    # Specify in-memory table as sink and start the stream in the background
-    query = streaming.writeStream.queryName("records")\
-        .format("memory").outputMode("append")\
-        .start()
+    if write:
+        colNames = ["JobTitle", "Company", "Location", "Url", "FingerPrint"]
+        getSoupUDF = udf(lambda x: getSoupDriver(x), StringType())
+        csvValue = streaming.select(streaming["value"].cast("string"))
+        pattern = r'"(.+?)","(.+?)","(.+?)","(.+?)","(.+?)"'
+        cols = [regexp_extract(csvValue["value"], pattern, 1).alias("JobTitle")
+            ,regexp_extract(csvValue["value"], pattern, 2).alias("Company")
+            ,regexp_extract(csvValue["value"], pattern, 3).alias("Location")
+            ,regexp_extract(csvValue["value"], pattern, 4).alias("Url")
+            ,regexp_extract(csvValue["value"], pattern, 5).alias("FingerPrint")]
+        separated = csvValue.select(*cols)
+        query = separated.withColumn("Posting", getSoupUDF(separated["Url"]))\
+            .writeStream.format("delta")\
+            .outputMode("append")\
+            .option("path", deltaPath)\
+            .option("checkPointLocation", checkPointLocation)\
+            .start()
+            # .writeStream.format("console")\
+            # .outputMode("append")\
+            # .start()
+        print(f"Writing data to Delta Lake at {deltaPath}")
 
-    while query.status["isTriggerActive"]:
-        print("{0} - {1}".format(query.status, spark.sql("SELECT * FROM records").count()))
-        time.sleep(1)
-    
-    # Prevent driver process from exiting while query is still running
-    query.awaitTermination()
+        # Terminate streaming query after 3 seconds of not receiving new data
+        timeoutNewData(query, 3)
+        query.stop()
+        query.awaitTermination()
+        print(f"Finished writing to Delta Lake at {deltaPath}")
+    else:
+        print(f"Delta lake at {deltaPath} already exists")
--- scripts/ss.ps1
+++ scripts/ss.ps1
@@ -1,14 +1,18 @@
 Clear-Host
 $packages = @(
-    "io.delta:delta-core_2.12:0.7.0",
-    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3"
+    "io.delta:delta-core_2.12:0.7.0",                   # to use Delta Lake
+    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3"  # to use Kafka as streaming source/sink
+) -join ","
+
+$jars = @(
+    "D:\Spark\sqljdbc_10.2\enu\mssql-jdbc-10.2.0.jre8.jar"  # to connect to SQL Server
 ) -join ","
 
 if($args.Length -gt 0)
 {
     spark-submit --master local `
         --packages $packages `
-        --jars "D:\Spark\sqljdbc_10.2\enu\mssql-jdbc-10.2.0.jre8.jar" `
+        --jars $jars `
         --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" `
         --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" `
         --conf "spark.sql.shuffle.partitions=5" `
